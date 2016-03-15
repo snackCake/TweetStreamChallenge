3efe0bb81ddd6572c916513b5149e8ed63a31d86
Stephen Hopper <shopper@shopper-osx.local>
2016-03-15 11:15:08 -0500

    Fixed issues with NPE happening deep in streams library whenever the batch size is reached.

--- app/services/TweetService.scala
+++ app/services/TweetService.scala
@@ -1,16 +1,14 @@
 package services
 
 import javax.inject._
-
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream._
 import akka.stream.scaladsl.Source
-import org.reactivestreams.{Publisher, Subscriber}
+import org.reactivestreams.{Subscription, Publisher, Subscriber}
 import play.api.{Configuration, Logger}
 import play.api.inject.ApplicationLifecycle
 import twitter4j._
-
 import scala.collection.mutable
 import scala.concurrent.Future
 
@@ -22,63 +20,77 @@ class TweetService @Inject()(config: Configuration, appLifecycle: ApplicationLif
 
   Logger.info(s"TweetService: Starting application.")
 
-  private lazy val searchTerms = config.getStringSeq("tweet.tags").getOrElse(Seq(""))
-  lazy val searchSource: Source[Status, NotUsed] = searchHashtags(searchTerms)
-
-  private lazy val tweetPublisher = new TweetPublisher
   private implicit val system = ActorSystem("tweet-service")
-
-
-  appLifecycle.addStopHook { () =>
-    tweetPublisher.shutdown()
-    Future.successful(())
-  }
 
   private implicit val materializer = ActorMaterializer()
   private val factory = new TwitterStreamFactory()
 
-  private def searchHashtags(searchString: Seq[String]): Source[Status, NotUsed] = {
+  def createSearchSource(searchString: Seq[String]): Source[Status, NotUsed] = {
     val query = new FilterQuery(searchString: _*)
     createTweetSource(query)
   }
 
   private def createTweetSource(query: FilterQuery): Source[Status, NotUsed] = {
-    tweetPublisher.twitter.filter(query)
+    val tweetPublisher = new TweetPublisher(query)
+
+    appLifecycle.addStopHook { () =>
+      tweetPublisher.shutdown()
+      Future.successful(())
+    }
+
     Source.fromPublisher(tweetPublisher)
   }
 
   private trait DuplicateCheckSubscriber[T] {
-    protected val subscribers = mutable.Buffer[Subscriber[_ >: T]]()
-    def subscribe(subscriber: Subscriber[_ >: T]): Unit =
-      if (!subscribers.contains(subscriber)) {
-        subscribers.append(subscriber)
+    this: Publisher[T] =>
+      protected val subscribers = mutable.Set[Subscriber[_ >: T]]()
+      override def subscribe(subscriber: Subscriber[_ >: T]): Unit = {
+        if (!subscribers.contains(subscriber)) {
+          subscribers.add(subscriber)
+        }
       }
   }
 
-  private class TweetPublisher extends Publisher[Status] with DuplicateCheckSubscriber[Status] {
+  private class TweetPublisher(query: FilterQuery) extends Publisher[Status] with DuplicateCheckSubscriber[Status] {
 
-    lazy val twitter = factory.getInstance()
+    val twitter = {
+      val t = factory.getInstance()
+      t.addListener(new StatusAdapter() {
+        override def onDeletionNotice(notice: StatusDeletionNotice) = Logger.warn(s"Status deletion notice: $notice")
+
+        override def onStallWarning(warning: StallWarning): Unit =
+          Logger.warn(s"Twitter stall warning: ${warning.getMessage} with code: ${warning.getCode}")
+
+        override def onException(ex: Exception): Unit = {
+          Logger.error(s"Twitter stream error:", ex)
+          subscribers.foreach { subscriber =>
+            subscriber.onError(ex)
+          }
+        }
+
+        override def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit =
+          Logger.warn(s"Twitter limitation notice, numberOfLimitedStatuses: $numberOfLimitedStatuses")
+
+        override def onStatus(status: Status): Unit = {
+          subscribers.foreach { subscriber =>
+            subscriber.onSubscribe(new Subscription {
+              override def cancel(): Unit = {
+                subscribers.remove(subscriber)
+              }
+              override def request(n: Long): Unit = {
+              }
+            })
+            subscriber.onNext(status)
+          }
+        }
+      })
+      t.filter(query)
+      t
+    }
 
     def shutdown() = {
       twitter.shutdown()
       subscribers.foreach { _.onComplete }
     }
-
-    twitter.addListener(new StatusAdapter() {
-      override def onStallWarning(warning: StallWarning): Unit =
-        Logger.warn(s"Twitter stall warning: ${warning.getMessage} with code: ${warning.getCode}")
-
-      override def onException(ex: Exception): Unit = {
-        Logger.error(s"Twitter error:", ex)
-        subscribers.foreach { subscriber =>
-          subscriber.onError(ex)
-        }
-      }
-
-      override def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit =
-        Logger.warn(s"Twitter limitation notice, numberOfLimitedStatuses: $numberOfLimitedStatuses")
-
-      override def onStatus(status: Status): Unit = subscribers.foreach { _.onNext(status) }
-    })
   }
 }
